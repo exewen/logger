4b632a8cf8075417cab0196ddc30b3dc27711781
exeweb <exeweb@163.com>
2024-12-31 17:00:03 +0800

    commit_message

--- src/Logger.php
+++ src/Logger.php
@@ -16,32 +16,46 @@ class Logger extends LoggerManager implements LoggerInterface
 
     public function debug($message, array $context = [])
     {
-        $this->driver(null, __FUNCTION__)->debug($message, $context);
+        $context = $this->formatContext($message, $context);
+        $this->driver(null, __FUNCTION__)->debug(null, $context);
     }
 
     public function request($message, array $context = [])
     {
-        $this->driver(null, __FUNCTION__)->info($message, $context);
+        $context = $this->formatContext($message, $context);
+        $this->driver(null, __FUNCTION__)->info(null, $context);
     }
 
     public function info($message, array $context = [])
     {
-        $this->driver(null, __FUNCTION__)->info($message, $context);
+        $context = $this->formatContext($message, $context);
+        $this->driver(null, __FUNCTION__)->info(null, $context);
     }
 
     public function notice($message, array $context = [])
     {
-        $this->driver(null, __FUNCTION__)->notice($message, $context);
+        $context = $this->formatContext($message, $context);
+        $this->driver(null, __FUNCTION__)->notice(null, $context);
     }
 
     public function warning($message, array $context = [])
     {
-        $this->driver(null, __FUNCTION__)->warning($message, $context);
+        $context = $this->formatContext($message, $context);
+        $this->driver(null, __FUNCTION__)->warning(null, $context);
     }
 
     public function error($message, array $context = [])
     {
-        $this->driver(null, __FUNCTION__)->error($message, $context);
+        $context = $this->formatContext($message, $context);
+        $this->driver(null, __FUNCTION__)->error(null, $context);
+    }
+
+    private function formatContext($message, array $context): array
+    {
+        $messageTemp = [
+            'message' => $message
+        ];
+        return $messageTemp + $context;
     }
 
 }
